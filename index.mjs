27b5ced99709fdcab99c04c9d689ed264a6b2eec
Fernando Reis Guimarães <fernandobhz@gmail.com>
2023-02-19 18:18:50 -0300

    initial commit

---
+++ index.mjs
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import fs from 'fs/promises';
+
+const log = (...args) => console.log(new Date().toISOString(), ...args);
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const currentPrevious = process.argv.at(2);
+
+if (!['current', 'previous'].includes(currentPrevious)) {
+  console.log(`Usage:\n\tnpx generate-log-files [current|previous]`);
+  process.exit(1);
+}
+
+log(`starting generation of ${currentPrevious} month log files`);
+
+const now = new Date();
+const currentYear = now.getFullYear();
+const currentMonth = now.getMonth() + 1;
+const workingMonth = currentMonth - (currentPrevious === 'previous' ? 1 : 0);
+
+const firstDate = new Date(currentYear, workingMonth, 1);
+const lastDate = new Date(currentYear, currentMonth, 0);
+
+const firstDay = firstDate.getDate();
+const lastDay = lastDate.getDate();
+
+for (let currentDay = firstDay; currentDay < lastDay; currentDay++) {
+  const currentDate = new Date(currentYear, currentMonth, currentDay)
+  const currentWeekDay = weekdays.at(currentDate.getDay());
+  const fileName = `${currentYear}-${currentMonth.toString().padStart(2, '0')}-${currentDay.toString().padStart(2, '0')} - ${currentWeekDay}.txt`;
+  
+  log(`creating ${fileName}`)
+  await fs.writeFile(fileName, ``);
+}
+
+log(`done`);
